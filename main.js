4f8fc3c1bc950bafae031e09d2ba5a1a758b1c66
Tusti-Gupta <86391913+Tusti-Gupta@users.noreply.github.com>
2023-10-23 11:02:07 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 shutDown = "";
 seven = "";
-queencard = "";
 leftWristX = 0;
 leftWristY = 0;
 rightWristX = 0;
 rightWristY = 0;
+scoreLeftWrist = 0;
+songStatus = "";    
 
 function preload() {
     shutDown = loadSound("Blackpink - Shut down.mp3");
     seven = loadSound("Seven - jungkook.mp3");
-    queencard = loadSound("GIDLE - Queencard.mp3");
 }
 
 function setup() {
@@ -29,11 +29,27 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 600, 500);
+
+    fill("#FFOOOO");
+    stroke("#FFOOOO");
+
+    if (scoreLeftWrist > 0.2) {
+        songStatus = shutDown.isPlaying();
+        circle(leftWristX, leftWristY, 20);
+        seven.stop();
+
+        if (songStatus = "false") {
+            shutDown.play();
+            document.getElementById("songName").innerHTML = "SHUTDOWN By BLACKPINK is playing";
+        }
+    }
 }
 
 function gotPoses(results) {
     if(results.length > 0) {
         console.log(results);
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
         console.log("leftWristX = " + leftWristX + " leftWristY = "+ leftWristY);
